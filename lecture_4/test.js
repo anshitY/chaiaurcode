fec3e3aac833d672c9fc5a524990272109d82b38
anshit <21uec026@lnmiit.ac.in>
2024-07-31 13:33:17 +0530

    lecture_4

---
+++ lecture_4/test.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let abc = "anb@gmail.com"
+var pass = "23233"
+accountCity = "jaipur"
+
+accountemail = "anshit@gmail.com"
+pass = "342342"
+accountCity= "gurgaon"
+
+let dummy;
+console.table([abc,pass,accountCity,accountemail,dummy])
