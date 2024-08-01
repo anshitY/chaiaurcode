919942b26ceaa526a07ce31fb2fe663fcb3b8321
anshit <21uec026@lnmiit.ac.in>
2024-08-01 05:35:14 +0530

    cmmt

--- lecture_5/test.js
+++ lecture_5/test.js
@@ -1,1 +1,15 @@
 "use strict"; // treat all js code as newer version
+let name = "anshit";
+let age = 18;
+let isLoggedIn = false;
+
+// number => 2^53
+// BigInt
+// string =>""
+// boolean =>true/false
+// null => standalone value'
+// undefine=>
+// symbol=>unique
+
+console.log(typeof undefined);
+console.log(typeof null);
